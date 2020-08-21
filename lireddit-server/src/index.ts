9ad33ed0dcc30ed122540cc6a79dc88ddb0937db
ulises <ulisescamps9@gmail.com>
2020-08-20 20:50:35 -0500

    Apollo server Express Setup

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -2,14 +2,29 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
 const main = async() => { 
  const orm = await MikroORM.init(mikroConfig)
  await orm.getMigrator().up()
- // const post = orm.em.create(Post, {title: 'my first post'})
- // await orm.em.persistAndFlush(post)
  
- //const posts = await orm.em.find(Post, {})
- //console.log(posts);
+ const app = express()
+ 
+ const apolloServer = new ApolloServer({
+     schema: await buildSchema({
+         resolvers: [HelloResolver],
+         validate: false
+     })
+ })
+
+ apolloServer.applyMiddleware({ app })
+
+ app.listen(4000, () => {
+     console.log('Server started on localhost:4000'); 
+ })
  
 };
 
