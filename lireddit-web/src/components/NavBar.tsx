e6f01e4b430b46f6a24f5e65a0ddaa188678298b
ulises <ulisescamps9@gmail.com>
2020-08-22 03:52:45 -0500

    Building login page and NavBar

---
+++ lireddit-web/src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Link, Flex, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="blue.50" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { NavBar } from "../components/NavBar";
 
 const Index = () => (
-  <div>Hello</div>
-)
+  <>
+    <NavBar />
+    <div>Hello</div>
+  </>
+);
 
-export default Index
+export default Index;
