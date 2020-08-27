2faddc9a5725f56d662f9b0b27d15b3850d9bc30
ulises <ulisescamps9@gmail.com>
2020-08-26 20:09:44 -0500

    urql-pagination

--- lireddit-web/src/pages/index.tsx
+++ lireddit-web/src/pages/index.tsx
@@ -4,16 +4,19 @@ import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
+import { useState } from "react";
 
 const Index = () => {
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
+  });
   const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 50,
-    },
+    variables,
   });
 
   if (!fetching && !data) {
-    return <div> you got query failed for dome reason </div>
+    return <div> you got query failed for dome reason </div>;
   }
   return (
     <Layout>
@@ -38,7 +41,17 @@ const Index = () => {
       )}
       {data ? (
         <Flex>
-          <Button isLoading={fetching} m="auto" my={8}>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts[data.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
             load more
           </Button>
         </Flex>
--- lireddit-web/src/utils/createUrqlClient.ts
+++ lireddit-web/src/utils/createUrqlClient.ts
@@ -1,5 +1,10 @@
-import { cacheExchange } from "@urql/exchange-graphcache";
-import { dedupExchange, fetchExchange, Exchange } from "urql";
+import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
+import {
+  dedupExchange,
+  fetchExchange,
+  Exchange,
+  stringifyVariables,
+} from "urql";
 import {
   LoginMutation,
   LogoutMutation,
@@ -22,6 +27,80 @@ const errorExhange: Exchange = ({ forward }) => (ops$) => {
   );
 };
 
+const cursorPagination = (): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+    const allFields = cache.inspectFields(entityKey);
+
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    const isItInTheCache = cache.resolveFieldByKey(entityKey, fieldKey);
+    info.partial = !isItInTheCache;
+
+    const results: string[] = [];
+    fieldInfos.forEach((fi) => {
+      const data = cache.resolveFieldByKey(entityKey, fi.fieldKey) as string[];
+      results.push(...data);
+    });
+    return results;
+    /* const visited = new Set();
+    let result: NullArray<string> = [];
+    let prevOffset: number | null = null;
+
+    for (let i = 0; i < size; i++) {
+      const { fieldKey, arguments: args } = fieldInfos[i];
+      if (args === null || !compareArgs(fieldArgs, args)) {
+        continue;
+      }
+
+      const links = cache.resolveFieldByKey(entityKey, fieldKey) as string[];
+      const currentOffset = args[offsetArgument];
+
+      if (
+        links === null ||
+        links.length === 0 ||
+        typeof currentOffset !== "number"
+      ) {
+        continue;
+      }
+
+      if (!prevOffset || currentOffset > prevOffset) {
+        for (let j = 0; j < links.length; j++) {
+          const link = links[j];
+          if (visited.has(link)) continue;
+          result.push(link);
+          visited.add(link);
+        }
+      } else {
+        const tempResult: NullArray<string> = [];
+        for (let j = 0; j < links.length; j++) {
+          const link = links[j];
+          if (visited.has(link)) continue;
+          tempResult.push(link);
+          visited.add(link);
+        }
+        result = [...tempResult, ...result];
+      }
+
+      prevOffset = currentOffset;
+    }
+
+    const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+    if (hasCurrentPage) {
+      return result;
+    } else if (!(info as any).store.schema) {
+      return undefined;
+    } else {
+      info.partial = true;
+      return result;
+ */
+  };
+};
+
 export const createUrqlClient = (ssrExchange: any) => ({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
@@ -30,6 +109,11 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      resolvers: {
+        Query: {
+          posts: cursorPagination(),
+        },
+      },
       updates: {
         Mutation: {
           logout: (_result, args, cache, info) => {
