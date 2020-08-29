d6e61a3944b566003d22350e2caa91bd85c33814
ulises <ulisescamps9@gmail.com>
2020-08-29 14:32:58 -0500

    updoot-ui

---
+++ lireddit-web/src/components/UpdootSection.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Flex, IconButton } from "@chakra-ui/core";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "updoot-loading" | "downdoot-loading" | "not-loading"
+  >("not-loading");
+  const [, vote] = useVoteMutation();
+  return (
+    <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
+      <IconButton
+        onClick={async () => {
+          setLoadingState("updoot-loading");
+          await vote({
+            postId: post.id,
+            value: 1,
+          });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "updoot-loading"}
+        icon="chevron-up"
+        aria-label="updoot post"
+      />
+      {post.points}
+      <IconButton
+        onClick={async () => {
+          setLoadingState("downdoot-loading");
+          await vote({
+            postId: post.id,
+            value: -1,
+          });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "downdoot-loading"}
+        icon="chevron-down"
+        aria-label="updown post"
+      />
+    </Flex>
+  );
+};
